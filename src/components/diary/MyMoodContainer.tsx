217aeb9c72e962831d3c82002f446e9957fcb3aa
ITHPARK <ithpark1035@gmail.com>
2024-12-21 22:06:00 +0900

    design : 아이콘 이미지 추가

--- src/components/diary/MyMoodContainer.tsx
+++ src/components/diary/MyMoodContainer.tsx
@@ -4,18 +4,27 @@ import styled from '@emotion/styled'
 
 const MyMoodContainer = ({ mood }: { mood: string }) => {
     return (
-        <MoodContainer justify="space-between">
-            <Text typography="t2" color="gray800">
-                오늘 당신의 감정은
-            </Text>
-            <Text typography="t2" color="gray800" weight="bold">
-                {mood}
-            </Text>
+        <MoodContainer>
+            <MoodBox justify="space-between">
+                <Text typography="t2" color="gray800">
+                    오늘 당신의 감정은
+                </Text>
+                <Text typography="t2" color="gray800" weight="bold">
+                    {mood}
+                </Text>
+            </MoodBox>
         </MoodContainer>
     )
 }
 
-const MoodContainer = styled(Flex)`
+const MoodContainer = styled.div`
+    padding-top: 36px;
+    background: url(/images/character/mood_character.png) no-repeat right 10px
+        top;
+    background-size: 60px auto;
+`
+
+const MoodBox = styled(Flex)`
     padding: 16px 30px 16px 20px;
     width: 100%;
     background: var(--gray100);
